2ee7a99421343d800f5e86a92ac9a7fec62cd167
Thitikorn Sangissarapirak <ramenguykung@gmail.com>
2024-10-11 10:26:13 +0700

    Implement SpeedInsights

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function Home() {
   return (
     <div className={styles.page}>
+    <SpeeeInsights/>
       <main className={styles.main}>
         <Image
           className={styles.logo}
